955ddea8d01404f3891850c16710f19b7a6d01d4
mtrkali <tarakfeni95@gmail.com>
2025-09-14 07:28:08 +0600

    added get single food api

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
         res.send(result)
     })
 
+    //get Single Food --
+    app.get('/foods/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId (id)}
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
 
